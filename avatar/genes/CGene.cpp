7821050f651145864efbdf97c6ef465907bb4ed5
Kamalyesh Kannadkar <kkamalyesh@gmail.com>
2020-02-09 17:35:10 +0530

    modified so that it is now base class

--- avatar/genes/CGene.cpp
+++ avatar/genes/CGene.cpp
@@ -1,6 +1,3 @@
-#include <iostream>
-#include <string>
-
 #ifndef __C__GENE_H
 #define __C__GENE_H
 
@@ -8,55 +5,25 @@
 #include "./SPoints.cpp"
 #include "./ELevel.cpp"
 
-
-using namespace std;
-
 class CGene
 {
 
-private:
+protected:
     struct sPoints _points;
     float _height;
     eSpecies _species;
     eLevel _level;
-    float getAverageHeightOf(eSpecies species)
-    {
-        switch (species)
-        {
-        case eSpecies::NONE:
-            return 1.0f;
-        default:
-        case eSpecies::HUMAN:
-            return 5.7f;
-        }
-    }
 
 public:
     CGene()
     {
-        _species = eSpecies::HUMAN;
-        _height = getAverageHeightOf(_species);
+        _species = eSpecies::SPECIES_NONE;
+        _level = eLevel::LEVEL_NONE;
+        _height = 0.0f;
         _points = SCPointsFactory::getInitialPoints(_species);
-        _level = eLevel::BEGINNER;
     }
-
-    void getInfo()
-    {
-        std::cout << "----- GENE INFORMATION -----\n";
-        std::cout << "----- Species: " << SCSpeciesInfo::getSpeciesName(_species) << "\n";
-        std::cout << "----- Height: " << _height << "\n";
-        std::cout << "----- Level: " << _level << "\n";
-        std::cout << "----- Points(at current level): \n\tHP-" << _points.health_points << "\tMP-" << _points.magic_points << "\tIP-" << _points.intelligence_points << "\n";
-        std::cout << "----- Minimum Points(at current level):\n\t HP-" << SCPointsFactory::getMinimumPoints(_species, _level).health_points << "\tMP-" << SCPointsFactory::getMinimumPoints(_species, _level).magic_points << "\tIP-" << SCPointsFactory::getMinimumPoints(_species, _level).intelligence_points << "\n";
-        std::cout << "----- Maximum Points(at current level):\n\t HP-" << SCPointsFactory::getMaximumPoints(_species, _level).health_points << "\tMP-" << SCPointsFactory::getMaximumPoints(_species, _level).magic_points << "\tIP-" << SCPointsFactory::getMaximumPoints(_species, _level).intelligence_points << "\n";
-        std::cout << std::endl;
-    }
+    void modify(eSpecies, eLevel) {}
+    void getInfo() {}
 };
 
 #endif
-
-main()
-{
-    CGene newGene;
-    newGene.getInfo();
-}
