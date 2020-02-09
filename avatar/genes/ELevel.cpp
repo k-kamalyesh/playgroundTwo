ff37d22517072de3045d520ecd0458e542abf91c
Kamalyesh Kannadkar <kkamalyesh@gmail.com>
2020-02-09 17:33:33 +0530

    fixed levels

--- avatar/genes/ELevel.cpp
+++ avatar/genes/ELevel.cpp
@@ -1,11 +1,34 @@
+#include <string>
+
 #ifndef __E__LEVEL_H
 #define __E__LEVEL_H
 
 enum eLevel
 {
-    BEGINNER,
-    INTERMEDIATE,
-    ADVANCED
+    LEVEL_NONE,
+    LEVEL_BEGINNER,
+    LEVEL_INTERMEDIATE,
+    LEVEL_ADVANCED
+};
+
+class SLeveleFactory
+{
+public:
+    static std::string getLevelName(eLevel level)
+    {
+        switch (level)
+        {
+        case LEVEL_BEGINNER:
+            return "BEGINNER";
+        case LEVEL_INTERMEDIATE:
+            return "INTERMEDIATE";
+        case LEVEL_ADVANCED:
+            return "ADVANCED";
+        case LEVEL_NONE:
+        default:
+            return "NONE";
+        }
+    }
 };
 
 #endif
